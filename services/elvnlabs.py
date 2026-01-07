fcc52227ead1d12f3963be92be0917e0949dc664
Siddique Khan <project.purpose.rollno.29@gmail.com>
2026-01-07 22:02:40 +0530

    async test

--- services/elvnlabs.py
+++ services/elvnlabs.py
@@ -1,14 +1,33 @@
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
+from elevenlabs.client import AsyncElevenLabs, ElevenLabs
+from elevenlabs.play import play, stream
+import asyncio
 import os
+from services.utils import play_audio_from_chunk
 
 load_dotenv()
 
-elevenlabs = ElevenLabs(
-  api_key=os.getenv("ELEVENLABS_API_KEY"),
+elevenlabs = AsyncElevenLabs(
+    api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
+
 def synthesize_and_play_speech(text: str, voice_id: str):
-    audio =  elevenlabs.text_to_speech.convert(text=text, voice_id=voice_id)
-    play(audio)
+    if isinstance(elevenlabs, ElevenLabs):
+        audio = elevenlabs.text_to_speech.convert(text=text, voice_id=voice_id)
+        play(audio)
+    else:
+        raise ValueError("Elevenlabs client instance is async not sync.")
+
+
+async def asynthesize_and_play_speech(text: str, voice_id: str):
+    pass
+
+    
+    # speech = elevenlabs.text_to_speech.stream(voice_id=voice_id, text=text)
+    # await stream()
+
+async def main():
+    await asynthesize_and_play_speech(voice_id="6WjhCXzqp2hnSqFtrG8P", text='this is a test ')
+
+asyncio.run(main())
--- services/utils.py
+++ services/utils.py
@@ -1,12 +1,12 @@
 from services.redis import RedisStore
 
 
-store = RedisStore()
 def add_message_from_console(session_id: str):
+    store = RedisStore()
     msg = input("Enter your message: ")
     store.add_user_message(session_id, msg)
 
-# while True:
+
 #     add_message_from_console("session_123")
 
-store.stop_session(session_id="session_123")
+# store.stop_session(session_id="session_123")

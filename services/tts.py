e16fdbe2ff6a3af5c0d5861ce2ef8d131e03ebfb
Siddique Khan <project.purpose.rollno.29@gmail.com>
2026-01-22 20:41:35 +0530

    asynchronous wrapper for pocket tts

--- services/tts.py
+++ services/tts.py
@@ -3,7 +3,6 @@ from pocket_tts import TTSModel
 from concurrent.futures import ThreadPoolExecutor
 
 
-tts_model = TTSModel.load_model()
 
 
 class TextToSpeechService:
@@ -13,21 +12,17 @@ class TextToSpeechService:
     
     async def asynthesize_speech(self, text: str, voice_id: str="alba"):
 
-        def generate():
-            return self.model.generate_audio_stream(model_state=self.model.get_state_for_audio_prompt(voice_id), text_to_generate=text)
-        
+        gen = self.model.generate_audio_stream(model_state=self.model.get_state_for_audio_prompt(voice_id), text_to_generate=text)
         loop = asyncio.get_event_loop()
-        audio_data = await loop.run_in_executor(self.executor, generate)
-        for chunk in audio_data:
+        def generate_next_chunk(gen):
+            try:
+                chunk = next(gen)
+                return chunk, False
+            except StopIteration:
+                return None, True
+        
+        while True:
+            chunk, done = await loop.run_in_executor(self.executor, generate_next_chunk, gen)
+            if done:
+                break
             yield chunk
-
-    
-async def main():
-    tts = TextToSpeechService()
-    text = """sample text"""
-    async for i in tts.asynthesize_speech(text):
-        print("Executed first instance")
-    async for j in tts.asynthesize_speech(text):
-        print("Executed second instance")
-
-asyncio.run(main=main())
